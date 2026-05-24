eab6011975cee4cd170908f3cc04ec11440e2c11
Abndx <abindasap669@gmail.com>
2026-05-24 15:36:31 +0530

    Added manual COPY INTO command and JSON unmarshalling queries

--- snowflake.sql
+++ snowflake.sql
@@ -1,13 +1,15 @@
--- 3. Create a table for the raw JSON
-CREATE OR REPLACE TABLE weather_raw (
-    v variant
-);
+-- 5. Manual load for testing
+COPY INTO weather_raw
+FROM @weather_stage
+FILE_FORMAT = (TYPE = 'JSON');
 
--- 4. Create the Stage pointing to S3
-CREATE OR REPLACE STAGE weather_stage
-  URL = 's3://weather-data-pipeline-landing-zone/raw-data/'
-  STORAGE_INTEGRATION = s3_weather_integration;
-
--- Check if Snowflake can see your files
-LIST @weather_stage;
-
+-- 6. Parse nested JSON and convert timestamps
+SELECT 
+    v:location::string as city,
+    v:weather[0].M.description.S::string as description,
+    -- This converts the Unix timestamp inside your JSON to a readable format
+    TO_TIMESTAMP_NTZ(v:timestamp::int) as utc_observation_time,
+    -- This converts that timestamp to your local IST time
+    CONVERT_TIMEZONE('UTC', 'Asia/Kolkata', TO_TIMESTAMP_NTZ(v:timestamp::int)) as local_ist_time
+FROM weather_raw 
+ORDER BY local_ist_time;
