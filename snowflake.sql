7647cda2990eacb2f35f48d7403205f32542669b
Abndx <abindasap669@gmail.com>
2026-05-24 15:52:31 +0530

    Step 2: Created Variant Table and S3 External Stage

--- snowflake.sql
+++ snowflake.sql
@@ -15,3 +15,17 @@ CREATE OR REPLACE STORAGE INTEGRATION s3_weather_integration
   STORAGE_AWS_ROLE_ARN = 'SNOWFLAKE_ROLE_ARN';
 
 DESC INTEGRATION s3_weather_integration;
+
+
+-- 3. Create a table for the raw JSON
+CREATE OR REPLACE TABLE weather_raw (
+    v variant
+);
+
+-- 4. Create the Stage pointing to S3
+CREATE OR REPLACE STAGE weather_stage
+  URL = 's3://weather-data-pipeline-landing-zone/raw-data/'
+  STORAGE_INTEGRATION = s3_weather_integration;
+
+-- Check if Snowflake can see your files
+LIST @weather_stage;
