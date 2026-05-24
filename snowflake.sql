7374877a2966e7ffa97f51721c61267bb4e5e2f3
Abndx <abindasap669@gmail.com>
2026-05-24 15:53:26 +0530

    Step 4: Configured Snowpipe for real-time S3 auto-ingestion

--- snowflake.sql
+++ snowflake.sql
@@ -47,3 +47,17 @@ SELECT
     CONVERT_TIMEZONE('UTC', 'Asia/Kolkata', TO_TIMESTAMP_NTZ(v:timestamp::int)) as local_ist_time
 FROM weather_raw 
 ORDER BY local_ist_time;
+
+
+-- ==========================================
+-- STEP 4: The Automation Engine (Snowpipe)
+-- ==========================================
+CREATE OR REPLACE PIPE weather_db.raw.weather_pipe
+AUTO_INGEST = TRUE
+AS
+COPY INTO weather_db.raw.weather_raw
+FROM @weather_stage
+FILE_FORMAT = (TYPE = 'JSON');
+
+SHOW PIPES;
+SELECT SYSTEM$PIPE_STATUS('weather_pipe');
