213db58fc3407d71cca588bd35e86419596215ed
Abndx <abindasap669@gmail.com>
2026-05-24 15:53:06 +0530

    Step 3: Added logic to unmarshal nested DynamoDB JSON

--- snowflake.sql
+++ snowflake.sql
@@ -29,3 +29,21 @@ CREATE OR REPLACE STAGE weather_stage
 
 -- Check if Snowflake can see your files
 LIST @weather_stage;
+
+
+
+
+-- ==========================================
+-- STEP 3: Manual Load & Data Parsing
+-- ==========================================
+COPY INTO weather_raw
+FROM @weather_stage
+FILE_FORMAT = (TYPE = 'JSON');
+
+SELECT 
+    v:location::string as city,
+    v:weather[0].M.description.S::string as description,
+    TO_TIMESTAMP_NTZ(v:timestamp::int) as utc_observation_time,
+    CONVERT_TIMEZONE('UTC', 'Asia/Kolkata', TO_TIMESTAMP_NTZ(v:timestamp::int)) as local_ist_time
+FROM weather_raw 
+ORDER BY local_ist_time;
