a15140e71ebf07ff0732e92089996c95ae518c69
Abndx <abindasap669@gmail.com>
2026-05-24 15:52:00 +0530

    Step 1: Created Database, Schema, and Storage Integration

--- snowflake.sql
+++ snowflake.sql
@@ -1,12 +1,17 @@
--- ==========================================
--- STEP 4: The Automation Engine (Snowpipe)
--- ==========================================
-CREATE OR REPLACE PIPE weather_db.raw.weather_pipe
-AUTO_INGEST = TRUE
-AS
-COPY INTO weather_db.raw.weather_raw
-FROM @weather_stage
-FILE_FORMAT = (TYPE = 'JSON');
+-- 1. Create Database and Schema
+CREATE DATABASE IF NOT EXISTS weather_db;
+CREATE SCHEMA IF NOT EXISTS weather_db.raw;
 
-SHOW PIPES;
-SELECT SYSTEM$PIPE_STATUS('weather_pipe');
+-- Tell this worksheet session to use them
+USE DATABASE weather_db;
+USE SCHEMA raw;
+
+-- 2. Setup AWS Storage Integration
+CREATE OR REPLACE STORAGE INTEGRATION s3_weather_integration
+  TYPE = EXTERNAL_STAGE
+  STORAGE_PROVIDER = 'S3'
+  ENABLED = TRUE
+  STORAGE_ALLOWED_LOCATIONS = ('s3://weather-data-pipeline-landing-zone/raw-data/')
+  STORAGE_AWS_ROLE_ARN = 'SNOWFLAKE_ROLE_ARN';
+
+DESC INTEGRATION s3_weather_integration;
